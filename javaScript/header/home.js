41a554241d9a2e1bf7a3a3b38a4eecfa42a7e9eb
PorkStew <stewartclay166@gmail.com>
2020-11-01 20:27:42 +0200

    added header and logo links

--- javaScript/header/home.js
+++ javaScript/header/home.js
@@ -3,8 +3,8 @@ const navLinks = document.querySelector(".nav-links");
 const links = document.querySelectorAll(".nav-links li");
 
 hamburger.addEventListener("click", () => {
-	navLinks.classList.toggle("open");
-	links.forEach(link => {
-		link.classList.toggle("fade");
-	});
+  navLinks.classList.toggle("open");
+  links.forEach(link => {
+    link.classList.toggle("fade");
+  });
 });
